877b8bd42d7e4bb239fc5a178ec648886f389a49
Aishwaryamalagatti <72919938+Aishwaryamalagatti@users.noreply.github.com>
2020-10-19 00:01:30 +0530

    third commit

---
+++ Mmd.java
@@ -0,0 +1,13 @@
+class Name{
+public static void main(String[]args){
+System.out.println("Aishwarya");
+music();
+drinking();
+}
+public static void music()
+{System.out.println(" I love music");
+}
+public static void drinking()
+{System.out.println("Orange Juice");
+}
+}
