68ae182121f5bf68291c17ba9ada8ac17b16942b
Aishwaryamalagatti <72919938+Aishwaryamalagatti@users.noreply.github.com>
2020-10-19 00:02:16 +0530

    fourth commit

---
+++ Methods.java
@@ -0,0 +1,21 @@
+class Five{
+public static void main(String[]args){
+System.out.println("Dance styles");
+first();
+second();
+third();
+fourth();
+}
+public static void first(){
+System.out.println("Bharatnatyam");
+}
+public static void second(){
+System.out.println("Hip-hop");
+}
+public static void third(){
+System.out.println("Free style");
+}
+public static void fourth(){
+System.out.println("Khathak");
+}
+}
