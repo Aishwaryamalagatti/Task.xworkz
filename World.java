66df71e2e4ca926f737f3f9961f4451d701384cc
Aishwaryamalagatti <72919938+Aishwaryamalagatti@users.noreply.github.com>
2020-10-19 23:50:26 +0530

    sixth commit

---
+++ World.java
@@ -0,0 +1,29 @@
+public class World{
+public static void main(String[]args)
+{
+System.out.println("Calculation");
+byte fingers;
+fingers=10;
+short daysInAYear;
+daysInAYear=365;
+int numberOfEngineersInIndia;
+ numberOfEngineersInIndia=50000;
+ long populationOfIndia;
+ populationOfIndia=423498745;
+ float percentageOfStudent;
+ percentageOfStudent=7.25f;
+ double chargeOfElectron;
+ chargeOfElectron=32.12345d;
+ boolean earthIsFlat;
+ earthIsFlat=false;
+ char grade;
+ grade='A';
+ System.out.println(fingers);
+ System.out.println(daysInAYear);
+ System.out.println(numberOfEngineersInIndia);
+ System.out.println(populationOfIndia);
+ System.out.println(percentageOfStudent);
+ System.out.println(chargeOfElectron);
+ System.out.println(earthIsFlat);
+System.out.println(grade);} }
+ 
