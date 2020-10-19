38cd760867c70bbb7544540db28021223b3bacba
Aishwaryamalagatti <72919938+Aishwaryamalagatti@users.noreply.github.com>
2020-10-19 23:52:49 +0530

    seventh commit

---
+++ Google.java
@@ -0,0 +1,26 @@
+public class Google{
+public static void main(String[]args)
+{
+System.out.println("Details");
+byte seasons;
+seasons=3;
+short numberOfWorkingDays=330;
+int numberOfOffices=70;
+long networth;
+networth=50000000;
+float numberOfCEO=1;
+double chargeOfProton=1.673d;
+boolean googleIsASearchEngine;
+googleIsASearchEngine=true;
+char position;
+position='A';
+System.out.println(seasons);
+System.out.println(numberOfWorkingDays);
+System.out.println(numberOfOffices);
+System.out.println(networth);
+System.out.println(numberOfCEO);
+System.out.println(chargeOfProton);
+System.out.println(googleIsASearchEngine);
+System.out.println(position);
+}
+}
