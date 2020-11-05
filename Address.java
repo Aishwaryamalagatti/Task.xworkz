b27dfb8f7269d44d0f06bdcdf9baf4d3d2efb0c9
Aishwaryamalagatti <72919938+Aishwaryamalagatti@users.noreply.github.com>
2020-11-06 00:28:22 +0530

    Fields-Nov5

---
+++ Address.java
@@ -0,0 +1,6 @@
+public class Address{
+public static short doorNo=100;
+public static String street="Vidyanagar";
+public static int pinCode=580021;
+public static String city="hubli";
+}
---
+++ HomeTester.java
@@ -0,0 +1,21 @@
+public class HomeTester{
+public static void main(String[] test)
+{
+String name=Home.Name;
+System.out.println(name);
+byte homeSize=Home.size;
+System.out.println(homeSize);
+int areaOfHome=Home.area;
+System.out.println(areaOfHome);
+short widthOfHome=Home.width;
+System.out.println(widthOfHome);
+long distanceCount=Home.distance;
+System.out.println(distanceCount);
+float heightOfHome=Home.height;
+System.out.println(heightOfHome);
+double homeCost=Home.cost;
+System.out.println(homeCost);
+boolean home=Home.myHome;
+System.out.println(home);
+}
+}
