3e064982536d853aae5fb656689ac092a9405a92
Aishwaryamalagatti <72919938+Aishwaryamalagatti@users.noreply.github.com>
2020-11-17 22:12:13 +0530

    Nov-13-20
    Eclipse

---
+++ BusSearch.java
@@ -0,0 +1,14 @@
+package com.xworkz.OnlinePgm;
+
+public class BusSearch {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jvm kareyuvudu main");
+
+		System.out.println("Total places to search" + Place.totalplaces());
+		Place.search("Banglore");
+
+		System.out.println("main nirgamana");
+	}
+
+}
---
+++ HotleMenuSearch.java
@@ -0,0 +1,14 @@
+package com.xworkz.OnlinePgm;
+
+public class HotleMenuSearch {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jvm kareyuvudu main");
+
+		System.out.println("Total foodItems to search" + HotelMenu.totalfoodItems());
+		HotelMenu.search("Dosa");
+
+		System.out.println("main nirgamana");
+	}
+
+}
